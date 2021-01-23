1249910045e44cdead117d97b67ddf126e25a23a
iancu vlad <iancuvladalexandru@gmail.com>
2021-01-24 01:02:07 +0200

    SSL Configuration

--- index.js
+++ index.js
@@ -2,13 +2,11 @@ let express = require('express')
 let https = require('https')
 let app = express()
 let authRouter = require('./api/auth').authRouter
+let ssl = require('./ssl')
 let files = require('fs')
 let path = require('path')
 app.use(authRouter);
 
-https.createServer({
-    key: files.readFileSync('server.key'),
-    cert: files.readFileSync('server.cert')
-}, app).listen(process.env.PORT, () => {
+https.createServer(ssl.credentials, app).listen(process.env.PORT, () => {
     console.log("App listening on http://localhost");
 })
