34ab114406ca8be60901dd7548a6a564cbeed52b
iancu vlad <iancuvladalexandru@gmail.com>
2021-02-26 01:14:46 +0200

    Bugfix

--- src/auth/data.ts
+++ src/auth/data.ts
@@ -76,14 +76,14 @@ type ValidateParams = { id: number, phoneCode?: string, emailCode?: string }
 async function validateUser(db: Database, { id, phoneCode, emailCode }: ValidateParams): Promise<any> {
     if (phoneCode) {
         let code = (await db.query("SELECT * FROM user_validation WHERE code = ?", [phoneCode]))[0]
-        if (code.expiration > getUnixTime())
+        if (code.expiration < getUnixTime())
             throw "Phone code has expired"
         await db.query("UPDATE user SET phoneCode = ? WHERE id = ? AND phoneCode = ?", [null, id, phoneCode.toUpperCase()])
         await db.query("DELETE FROM user_validation WHERE code = ?", [phoneCode])
     }
     if (emailCode) {
         let code = (await db.query("SELECT * FROM user_validation WHERE code = ?", [emailCode]))[0]
-        if (code.expiration > getUnixTime())
+        if (code.expiration < getUnixTime())
             throw "Phone code has expired"
         await db.query("UPDATE user SET emailCode = ? WHERE id = ? AND emailCode = ?", [null, id, emailCode.toUpperCase()])
         await db.query("DELETE FROM user_validation WHERE code = ?", [emailCode])
